d407944da2feccc3717f79146dea2c8c68565e0d
Zenya Zelinsky <deimos3000@ukr.net>
2018-05-19 12:47:05 +0300

    2

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,19 @@
 $(function() {
 
-	// Пользовательские функции
+	// Пользовательские 
+	$('.chosen-select').chosen({
+		disable_search: true,
+	});
+	
+	var send = $('.dash_search_input');
+	$('.search').click(function(){
+		if($(send).val() !== ""){
+			// submit
+			alert($(send).val());
+		}
+	})
+
+
+	
 
 });
