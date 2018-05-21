cc50cf0e15b7afe1c0f39d3363910a43cf1e9ae2
Zenya Zelinsky <deimos3000@ukr.net>
2018-05-21 23:04:32 +0300

    done!

--- app/js/common.js
+++ app/js/common.js
@@ -18,5 +18,40 @@ $(function() {
 		loop: true
 	});
 	
+	$('.burger').on("click", function(){
+
+		if($(".burger").hasClass('open')){
+			$(".mob_menu").slideToggle("slow", function(){
+
+				$(".one").css( "transform","rotate(0)");
+				$(".one").css( "margin-bottom", "5px");
+				$(".one").css( "bottom", "0px");
+				$(".two").css( "opacity", "1");
+				$(".three").css( "transform","rotate(0deg)");
+				$(".three").css( "margin-bottom", "5px");
+				$(".three").css( "bottom", "0px");
+				$(".burger").removeClass('open');
+				
+			})
+
+		} else {
+			$(".mob_menu").slideToggle("slow", function(){
+				$(".one").css( "transform","rotate(45deg)", "margin-bottom", "0");
+				$(".one").css( "margin-bottom", "-11px");
+				$(".one").css( "bottom","10px");
+				$(".two").css( "opacity", "0");
+				$(".three").css( "transform","rotate(-45deg)", "margin-bottom", "0");
+				$(".three").css( "margin-bottom", "0");
+				$(".three").css( "bottom","10px");
+				$(".burger").addClass('open');
+			})	
+		}
+
+	})
+	$('.sub-menu ul').hide();
+		$(".sub-menu a").click(function () {
+		$(this).parent(".sub-menu").children("ul").slideToggle("100");
+		$(this).find(".right").toggleClass("fa-caret-up fa-caret-down");
+	});
 
 });
